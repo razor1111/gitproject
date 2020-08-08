3122d977731f47dad7a9a4c28275236ca94b6c6e
Neto Rodrigues <netinho-3003@hotmail.com>
2020-08-08 13:16:05 -0400

    animações do personagem

--- New Unity Project/Assets/Scripts/player.cs
+++ New Unity Project/Assets/Scripts/player.cs
@@ -11,12 +11,14 @@ public bool isJumping;
 public bool doubleJump;
 
 private Rigidbody2D rig;
+private Animator anim;
 
 
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -30,6 +32,25 @@ private Rigidbody2D rig;
     {
         Vector3 moviment = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += moviment * Time.deltaTime * Speed;
+
+        if(Input.GetAxis("Horizontal") > 0f)
+        {
+            anim.SetBool("walk",true);
+            transform.eulerAngles = new Vector3(0f,0f,0f);
+        }
+
+        if(Input.GetAxis("Horizontal") < 0f)
+        {
+            anim.SetBool("walk",true);
+            transform.eulerAngles = new Vector3(0f,180f,0f);
+        }
+
+        if(Input.GetAxis("Horizontal") == 0f)
+        {
+            anim.SetBool("walk",false);
+        }
+
+
     }
 
     void Jump()
@@ -40,6 +61,8 @@ private Rigidbody2D rig;
             {
                 rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
                 doubleJump = true;
+                anim.SetBool("jump",true);
+
             }
             else
             {
@@ -58,6 +81,8 @@ private Rigidbody2D rig;
         if(collision.gameObject.layer == 8)
         {
             isJumping = false;
+            anim.SetBool("jump",false);
+
         }
     }
 
