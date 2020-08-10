fa4747dba10f26f4dd322d1c395ceac088f8b1f0
Neto Rodrigues <netinho-3003@hotmail.com>
2020-08-10 00:08:17 -0400

    Configurando inimigo

--- New Unity Project/Assets/Scripts/Frog.cs
+++ New Unity Project/Assets/Scripts/Frog.cs
@@ -4,64 +4,67 @@ using UnityEngine;
 
 public class Frog : MonoBehaviour
 {
-    private Rigidbody2D rigBody;
-    private Animator animation;
+
+    private Rigidbody2D rig;
+    private Animator anim;
+    
+    public float speed;
+
+    public Transform rightCol;
+    public Transform leftCol;
+    
+    public Transform headPoint;
+    
     private bool colliding;
 
-    public float speed;
-    public Transform rightCollision;
-    public Transform leftCollision;
-    public Transform topCollision;
     public LayerMask layer;
-
+    
     public BoxCollider2D boxCollider2D;
     public CircleCollider2D circleCollider2D;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        rigBody = GetComponent<Rigidbody2D>();
-        animation = GetComponent<Animator>();
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        rigBody.velocity = new Vector2(speed, rigBody.velocity.y);
+        rig.velocity = new Vector2(speed, rig.velocity.y);
 
-        colliding = Physics2D.Linecast(rightCollision.position, leftCollision.position, layer);
+        colliding = Physics2D.Linecast(rightCol.position, leftCol.position);
 
         if(colliding)
         {
             transform.localScale = new Vector2(transform.localScale.x * -1f, transform.localScale.y);
-            speed = -speed;
+            speed *= -1f;
+            
+            
+        }
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            float height = col.contacts[0].point.y - headPoint.position.y;
+        
+
+        if(height > 0)
+        {
+                col.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                speed = 0;
+                anim.SetTrigger("die");
+                boxCollider2D.enabled = false;
+                circleCollider2D.enabled = false;
+                rig.bodyType = RigidbodyType2D.Kinematic;
+                Destroy(gameObject, 0.33f);
         }
     }
 
-    bool playerDestroyed;
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.tag == "Player")
-        {
-            float height = collision.contacts[0].point.y - topCollision.position.y;
-
-            if(height > 0 && !playerDestroyed)
-            {
-                collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 10, ForceMode2D.Impulse);
-                speed = 0;
-                animation.SetTrigger("die");
-                boxCollider2D.enabled = false;
-                circleCollider2D.enabled = false;
-                rigBody.bodyType = RigidbodyType2D.Kinematic;
-                Destroy(gameObject, 0.42f);
-            }
-            else
-            {
-                playerDestroyed = true;
-                GameController.instance.ShowGameOver();
-                Destroy(collision.gameObject);
-            }
-
-        }
     }
 }
